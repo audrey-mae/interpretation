376af2d8a38ef59649dddc29dcc9cdb1c7852a37
audrey-mae <55456111+audrey-mae@users.noreply.github.com>
2019-10-01 08:54:54 -0700

    Add files via upload

---
+++ reference.js
@@ -0,0 +1,58 @@
+// // // stroke(255, 0, 0)
+// // // 	fill(255, 0, 0)
+// // // 	rect(0, 0, 400, 300)
+
+// // // 	line(0, 0, 400, 300)
+// // // 	line(0, 300, 400, 0)
+
+// // // 	// (r, g, b, a, arcstart, arcend)
+// // // 	// a = opacity (255=full; 0=transparent)
+// // // 	// 0 degrees is at 3 o clock - to the right 
+
+
+// // // 	ellipse(200, 150, 50, 50)
+
+// // // 	strokeWeight(5)
+// // // 	line(0, 150, 400, 150)
+
+// // // //comment 
+
+// // // print("everything is ok")
+
+// // noStroke()
+
+// // angleMode(DEGREES)
+
+// // fill(0, 0, 255)
+// // arc(250, 105, 200, 200, 90, 180)
+
+// // fill(255, 0, 0)
+// // arc(250, 105, 200, 200, 180, 270)
+
+// // fill(0, 255, 0)
+// // arc(250, 105, 200, 200, 270, 0)
+
+// // fill(10, 10, 10)
+// // arc (250, 105, 200, 200, 0, 90)
+
+
+// beginShape() 
+// 	vertex(45, 116)
+// 	vertex(99, 41)
+// 	vertex(138, 124)
+// 	vertex(46, 116)
+// endShape()
+
+// beginShape() - the beginning and end point shows where the curve is aiming itself- but it doesn't TOUCH that point 
+// 	curveVertex(45, 116)
+// 	curveVertex(45, 116)
+// 	curveVertex(99, 41)
+// 	curveVertex(138, 124)
+// 	curveVertex(46, 116)
+// 	curveVertex(46, 116)
+// endShape()
+
+
+//ellipse(width/2, height/2, 50, 50) = circle in middle of canvas with radius 50
+
+//translate(0,50) = move a thing 50 pixels 
